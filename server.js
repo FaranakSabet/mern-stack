6ed5e727d97e62fff4a9afd57cbee6bc143b7935
faranak Sabet <faranaksabet@yahoo.com>
2020-11-18 17:48:32 -0800

    adding cors config, dotenv and mongoose

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require('mongoose')
+const cors = require("cors")
+
+require("dotenv").config()
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +15,15 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+
+// CORS
+var corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
+
 // Define API routes here
 
 // Send every other request to the React app
@@ -19,6 +32,11 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+// connect to mongo db
+mongoose
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => {
+    app.listen(PORT)
+    console.log(`🌎 ==> API server now on port ${PORT}!`)
+  })
+  .catch((err) => console.log(err))
